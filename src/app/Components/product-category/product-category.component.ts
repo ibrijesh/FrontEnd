89444007732017bfbb164233026a5c1849cfa80e
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2025-01-05 14:53:34 +0530

    service is created

--- src/app/Components/product-category/product-category.component.ts
+++ src/app/Components/product-category/product-category.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductCategoryService } from '../../Services/product-category.service';
 
 @Component({
   selector: 'app-product-category',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './product-category.component.html',
   styleUrl: './product-category.component.css'
 })
-export class ProductCategoryComponent {
+export class ProductCategoryComponent implements OnInit{
 
+  data: any;
+  error : any
+
+  constructor(private productCategorySerivice: ProductCategoryService) {}
+
+  ngOnInit(): void {
+   
+   this.productCategorySerivice.GetProductCategory().subscribe({
+    next : (data) => {
+      this.data = data;
+    },
+    error: (err) => {
+      this.error = "its error"
+      console.error(err);
+    }
+   })
+    
+  }
 }
--- src/app/Services/product-category.service.ts
+++ src/app/Services/product-category.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductCategoryService {
 
-  constructor() { }
+  private url = "http://localhost:5147/api/productcategory";
 
-  
+  constructor(private http: HttpClient) { }
+
+  GetProductCategory() : Observable<any> {
+
+    return this.http.get<any>(this.url);
+
+  }
 
 }
